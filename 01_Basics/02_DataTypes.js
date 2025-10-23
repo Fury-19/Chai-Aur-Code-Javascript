7339158083f32871d676b7c8bde317326ede3ffd
Priyansh Dhukia <anshudhukia0002@gmail.com>
2025-10-23 21:51:21 +0530

    Datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all JS code as newer version!!
+
+// alert(3 + 3) => We can run this in browser only, not in vs code as here only nodeJs engine used!!
+
+// Code readability should be good for ensuring easier code reading for others!!
+
+// Basic Datatypes -> String, integer(number), boolean:-
+
+let name = "fury19" // String Datatype
+let age = 1 // Number Datatype
+let isLoggedIn = false; // Boolean Datatype
+
+// Primitive Datatypes:=)
+// Range of number datatype in js is upto 2 power 53 - 1 in both negative and positive direction!!
+// Bigint is also a datatype used to handle large numbers in bigger websites!!
+// String
+// Null -> Is also a datatype but it is a standalone value!! (A representation for empty value!!)
+// Undefined -> It refers that the value is not yet assigned to the variable!!
+// Symbol -> Used to refer Uniqueness!!
+
+console.log(typeof null) // Gives null as an object!!
+console.log(typeof undefined)
+
+// Object:=)
