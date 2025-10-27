acb008effdec65a2fb121e0012e4ffd8f7c61a6f
Priyansh Dhukia <anshudhukia0002@gmail.com>
2025-10-27 23:19:52 +0530

    Strings

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,22 @@
+const name = "Fury_19"
+const repoCount = 96
+
+console.log(name + repoCount + " Value") // Bad Syntax of writing code!!
+
+// Better Way of writing code!!
+console.log(`Hi, Iam ${name} my repo count on github is ${repoCount}`); // This is known as string interpolation!!
+
+let gameName = new String("fury19")
+
+console.log(gameName[0]);
+
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('y')); // Gives Index of first occurence passed as argument!!
+
+
+
+// Read and Learn about other functions related to String form MDN!!!
