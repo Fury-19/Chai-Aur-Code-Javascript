23c0a65c7d8c4aaaa6060ba1cf8b42206b26acd0
Priyansh Dhukia <anshudhukia0002@gmail.com>
2025-10-27 23:38:55 +0530

    Strings

--- 01_Basics/05_Strings.js
+++ 01_Basics/05_Strings.js
@@ -18,4 +18,4 @@ console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('y')); // Gives Index of first occurence passed as argument!!
 
-// Read and Learn about other functions related to String form MDN!!
+// Read and Learn about other functions related to String form MDN!!!
