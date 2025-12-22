f5dbbdad262c2a48ffcccfca2a2090a80a31d64d
Priyansh Dhukia <anshudhukia0002@gmail.com>
2025-12-22 10:11:39 +0530

    Conditionals DONE

---
+++ 04_Control Flow/Conditionals.js
@@ -0,0 +1,10 @@
+// Basic comparison operators:-
+// <, >, >=, <=, !=, ==, ===(Checks for data type too as compared to '=='), !==
+
+// 1: if Statement
+// 2: else if Statement
+// 3: else Statement
+
+// We can also use and(&&), or(||) between two conditions in if, else if statements!!
+// Take care of variable scopes in conditionals blocks of code!!
+// We can introduce mixture of '&&' and '||' conditions in conditionals!!
